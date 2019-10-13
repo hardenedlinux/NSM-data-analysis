c7ae1118f5962707496e0a853c2b26c931237b6c
GTrunsec <gtrunsec@hardenedlinux.org>
2019-10-12 22:08:21 -0700

    update NSM analysis env

---
+++ pkgs/python/bat/default.nix
@@ -0,0 +1,44 @@
+{ stdenv
+, python3Packages
+, fetchurl
+}:
+
+python3Packages.buildPythonPackage rec {
+
+  pname = "bat";
+  version = "v0.3.7";
+  src = fetchurl {
+   # url = "https://github.com/SuperCowPowers/${pname}/archive/v${version}.tar.gz";
+    url = "https://github.com/SuperCowPowers/bat/archive/${version}.tar.gz";
+    sha256 = "0ag7rvldfr741n5br50i85kh5mh3zqmrvwal31xyag846i6hh5zq";
+  };  
+  # src = fetchPypi {
+  #   inherit pname version;
+  #   sha256 = "13i7mqg14zp63d52lckslvzgxi75div7864rd1m56dqyc4kwxq7d";
+  # };
+  # src = fetchFromGitHub {
+  #   inherit pname
+  #   owner = "r9y9";
+  #   rev = "$version";
+  #   sha256 = "1yjhvakz0ai65i7n4ah5kakfgpp1inz9fghdvchhhl1a1s2scqfp";
+  # };
+  
+
+  propagatedBuildInputs = with python3Packages; [ pandas
+                                                  scikitlearn
+                                                   pyarrow
+                                                   requests
+                                                   watchdog
+                                                  numpy
+                                                  pyspark
+                                                 ];
+  doCheck = false;
+  
+  meta = with stdenv.lib; {
+    description = "Bro Analysis Tools (BAT): Processing and analysis of Bro network data with Pandas, scikit-learn, and Spark";
+    homepage = "https://github.com/SuperCowPowers/bat";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ gtrunsec ];
+  };
+
+}
---
+++ shell.nix
@@ -0,0 +1,72 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/GTrunSec/nixpkgs-channels/tarball/2436c27541b2f52deea3a4c1691216a02152e729";
+    sha256 = "0p98dwy3rbvdp6np596sfqnwlra11pif3rbdh02pwdyjmdvkmbvd";
+  };
+  # last update 
+  pkgs = import nixpkgs { config = {}; };
+  ## load static nixpkgs
+  #pkgs = import ~/src/nixpkgs-channels {};
+
+  ## add own-python package
+  bat = pkgs.callPackages ./pkgs/python/bat {};
+  editdistance = pkgs.callPackages ./pkgs/python/editdistance {};
+  IPy = pkgs.callPackages ./pkgs/python/IPy {};
+  networkx = pkgs.callPackages ./pkgs/python/networkx {};
+  netaddr = pkgs.callPackages ./pkgs/python/netaddr {};
+  tldextract = pkgs.callPackages ./pkgs/python/tldextract {};    
+  # Change these if you need extra R or Python packages.
+  my-R-packages = with pkgs.rPackages; [
+    summarytools
+    cmaes
+    ggplot2
+    dplyr
+    doBy
+    xts
+    anomalize
+  ];
+  my-python-packages = [ 
+    (pkgs.python3.withPackages (pkgs: with pkgs; [
+      setuptools
+      tldextract
+      bat
+      numpy
+      scikitlearn
+      netaddr
+      editdistance
+      IPy
+      matplotlib
+      sqlalchemy
+      pandas
+      networkx
+    ]))
+  ];
+
+  R-with-my-packages = pkgs.rWrapper.override{ packages = with pkgs.rPackages;
+    my-R-packages
+    ++ [ JuniperKernel]; };
+  jupyter-R-kernel = pkgs.stdenv.mkDerivation {
+    name = "jupyter-R-kernel";
+    buildInputs = [ pkgs.python37Packages.jupyter pkgs.python37Packages.notebook
+                    R-with-my-packages
+                    pkgs.which ];
+    unpackPhase = ":";
+    installPhase = ''
+    export HOME=$TMP
+    echo $JUPYTER_PATH
+    ${R-with-my-packages}/bin/R --slave -e "JuniperKernel::listKernels()"
+    ${R-with-my-packages}/bin/R --slave -e "JuniperKernel::installJuniper(prefix='$out')"
+  '';
+  };
+in
+pkgs.mkShell rec {
+  name = "jupyter-with-R-kernel";
+  buildInputs = [ jupyter-R-kernel
+                  pkgs.python37Packages.jupyter
+                ] ++ my-python-packages;
+  shellHook = ''
+    export JUPYTER_PATH=${jupyter-R-kernel}/share/jupyter
+    # see https://github.com/NixOS/nixpkgs/issues/38733
+    ${R-with-my-packages}/bin/R --slave -e "system2('jupyter', 'notebook')"
+  '';
+}
