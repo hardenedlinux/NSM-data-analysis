8d41731b28e6d54d5ab6cbf2e3a0525a5720ee92
GTrunsec <gtrunsec@hardenedlinux.org>
2020-04-06 15:48:33 -0700

    test

--- release-pkgs.nix
+++ release-pkgs.nix
@@ -1,12 +1,21 @@
 { ... }:
 let
   pkgs = (import <nixpkgs> {});
+  my-julia = (import ./pkgs/julia-non-cuda.nix {inherit pkgs;});
+  my-python = (import ./pkgs/python.nix {inherit pkgs;});
+  my-go =  (import ./pkgs/go.nix {inherit pkgs;});
+  my-R = (import ./pkgs/R.nix {inherit pkgs;});
 in {
   zeek = pkgs.callPackage ./pkgs/zeek {};
   broker = pkgs.callPackage ./pkgs/broker {};
   vast = pkgs.callPackage ./pkgs/vast {};
-  julia = (import ./pkgs/julia-non-cuda.nix {});
-  my-python = (import ./pkgs/python.nix {});
-  my-go =  (import ./pkgs/go.nix {});
-  my-R = (import ./pkgs/R.nix {});
+  nsm-data-analysis-pkgs = pkgs.buildEnv {
+  name = "nsm-data-analysis-pkgs";
+  paths = with pkgs; [
+    my-julia
+    my-go
+    my-R
+    my-python
+  ];
+  };
 }
