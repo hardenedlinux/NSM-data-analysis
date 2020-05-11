9d82e231bdf2a4b17ed8e81323fc4f9b8482d054
GTrunsec <gtrunsec@hardenedlinux.org>
2020-05-11 00:56:56 -0700

    update apache-airflow

--- pkgs/python.nix
+++ pkgs/python.nix
@@ -1,4 +1,5 @@
-{pkgs}:
+{ pkgs ? import ./ownpkgs.nix {}
+}:
 let
   beakerx = pkgs.callPackage ./python/beakerx {};
   cudf = pkgs.callPackage ./python/cudf {inherit rmm;};
@@ -31,6 +32,7 @@ let
   fastai = pkgs.callPackage ./python/fast-ai {};
   fastai2 = pkgs.callPackage ./python/fastai2 {};
   aiohttp = pkgs.callPackage ./python/aiohttp {};
+  own-airflow = pkgs.callPackage ./python/apache-airflow {};
   broker = pkgs.callPackage ./broker {};
   my-python-packages = (pkgs.python3.withPackages (ps: [ ps.jupyterlab
                                                          ps.pandas
@@ -88,6 +90,7 @@ let
                                                          ps.elasticsearch
                                                          ps.requests
                                                          yarapython
+                                                         own-airflow
                                                          #cudf ../include/rmm/detail/memory_manager.hpp:37:10: fatal error: rmm/detail/cnmem.h: No such file or directory
                                                          #axelrod pathlib 1.0.1 does not support 3.7
                                                        ])).override (args: { ignoreCollisions = true;});
@@ -103,6 +106,6 @@ pkgs.buildEnv rec {
           ];
   ignoreCollisions = true;
   postBuild = ''
-    #ln -s ${broker}/lib/python3.7/site-packages/broker/_broker.so $out/lib
+  wrapProgram $out/bin/airflow --set PYTHONPATH "$(echo ${my-python-packages}/lib/*/site-packages)"
 '';
 }
