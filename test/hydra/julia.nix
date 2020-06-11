c9dbee5f967a667f381fc24b127b78dcad54155d
GTrunsec <gtrunsec@hardenedlinux.org>
2020-06-10 17:08:21 -0700

    fix julia

--- test/hydra/julia.nix
+++ test/hydra/julia.nix
@@ -6,7 +6,7 @@ in
   Pkg-Julia = pkgs.buildEnv {
     name = "Julia";
     paths = with pkgs; [
-      (pkgs.julia_13.overrideAttrs(oldAttrs: {checkTarget = "";});)
+      (pkgs.julia_13.overrideAttrs(oldAttrs: {checkTarget = "";}))
     ];
   };
 }
