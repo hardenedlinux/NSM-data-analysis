182c81c56d68f0336e925bea176081e465650af5
GTrunsec <gtrunsec@hardenedlinux.org>
2020-06-11 14:23:40 -0700

    update zeek

---
+++ test/zeek.nix
@@ -0,0 +1,15 @@
+let
+  overlays = [
+    (import ../nix/packages-overlay.nix)
+  ];
+
+  pkgs = (import ../nix/nixpkgs.nix ) { inherit overlays;};
+in
+{
+  Env-zeek = pkgs.buildEnv {
+    name = "nsm-zeek";
+    paths = with pkgs; [
+      (zeek.override{ KafkaPlugin = true; PostgresqlPlugin = true;})
+    ];
+  };
+}
