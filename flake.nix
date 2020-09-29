1d884a7ddd6aa0d04fa05a5f6efd11e1d1d25552
GTrunSec <gtrunsec@hardenedlinux.org>
2020-09-29 15:59:24 -0700

    pre-test

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
-  description = "my project description";
+  description = "hardenedlinux nixpkgs collection";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    master.url = "nixpkgs/684d5d27136f154775c95005dcce2d32943c7c9e";
+    nixpkgs.url = "nixpkgs/684d5d27136f154775c95005dcce2d32943c7c9e";
   };
 
-  outputs = inputs@{ self, master, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
     {
-      overlay = import ./nix/python-packages-overlay.nix;
+      python-packages-overlay = import ./nix/python-packages-overlay.nix;
+      packages-overlay = import ./nix/packages-overlay.nix;
     }
     //
     (flake-utils.lib.eachDefaultSystem
         (system:
           let
-            pkgs = import master {
+            pkgs = import nixpkgs {
               inherit system;
               overlays = [
-                self.overlay
+                self.python-packages-overlay
+                self.packages-overlay
           ];
         };
           in
