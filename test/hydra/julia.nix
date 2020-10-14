0fcf9550bc73a48124c9af11ebfbac49797a4988
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-13 19:51:19 -0700

    pkgs: Bumpup julia to 1.5.1

--- test/hydra/julia.nix
+++ test/hydra/julia.nix
@@ -6,7 +6,13 @@ in
   Pkg-Julia = pkgs.buildEnv {
     name = "Julia";
     paths = with pkgs; [
-      (pkgs.julia_13.overrideAttrs(oldAttrs: {checkTarget = "";}))
+      (pkgs.julia_13.overrideAttrs(oldAttrs: {
+        src = pkgs.fetchzip {
+          url = "https://github.com/JuliaLang/julia/releases/download/v1.5.1/julia-1.5.1-full.tar.gz";
+          sha256 = "sha256-uaxlzni2RtmDhMzPbtDycj44CB0tJUzhmbwsAGwFv/U=";
+        };
+        checkTarget = "";}
+      ))
     ];
   };
 }
