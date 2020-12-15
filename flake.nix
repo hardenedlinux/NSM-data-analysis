13b9ac60911d434b3541d22c06d454ffe2a93098
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-14 21:01:51 -0800

    Do not check vast

--- flake.nix
+++ flake.nix
@@ -30,11 +30,39 @@
           };
         in
           {
-            devShell = import ./shell.nix { inherit pkgs;};
+            devShell = with pkgs; mkShell {
+              buildInputs = [
+                (pkgs.python37.withPackages (ps: with ps;[
+                  beakerx
+                  elastalert
+                  btest
+                  fastai
+                  zat
+                  cefpython3
+                  pyshark
+                  tldextract
+                  yarapython
+                  python-pptx
+                  jupyterlab_git
+                  jupyter_lsp
+                  fastai
+                  choochoo
+                  # cudf ../include/rmm/detail/memory_manager.hpp:37:10: fatal error: rmm/detail/cnmem.h: No such file or directory
+                  # axelrod pathlib 1.0.1 does not support 3.7
+                ]))
+                zeek
+                deepsea
+                nvdtools
+                sybilhunter
+                zq
+                vast
+              ];
+            };
             packages = {
               inherit (pkgs)
                 zeek
-                vast;
+                #vast
+              ;
             };
           }
       )
--- shell.nix
+++ shell.nix
@@ -1,34 +1,3 @@
-{ pkgs }:
-with pkgs;
-let
-      hardenedlinux-python-packages = (pkgs.python37.withPackages (ps: with ps;[
-            beakerx
-            elastalert
-            btest
-            fastai
-            zat
-            cefpython3
-            pyshark
-            tldextract
-            yarapython
-            python-pptx
-            jupyterlab_git
-            jupyter_lsp
-            fastai
-            choochoo
-            # cudf ../include/rmm/detail/memory_manager.hpp:37:10: fatal error: rmm/detail/cnmem.h: No such file or directory
-            # axelrod pathlib 1.0.1 does not support 3.7
-      ])).override (args: { ignoreCollisions = true;});
-
-in
-mkShell {
-      buildInputs = [
-            hardenedlinux-python-packages
-            #vast
-            zeek
-            deepsea
-            nvdtools
-            sybilhunter
-            zq
-      ];
-}
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = ./.;
+}).shellNix
