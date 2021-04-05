21912293ed23b55d1a011564f443c1686e764089
GTrunSec <gtrunsec@hardenedlinux.org>
2021-04-05 15:04:26 -0700

    pkgs: spicy add spicy-analyzers

--- pkgs/spicy/default.nix
+++ pkgs/spicy/default.nix
@@ -11,17 +11,25 @@
 , lib
 , makeWrapper
 , glibc
+, SpicyPlugin ? true
 }:
-
+let
+  spicy-analyzers = fetchFromGitHub {
+    owner = "zeek";
+    repo = "spicy-analyzers";
+    rev = "26696b65574faf6282aaa5e7836fda1f43a09092";
+    sha256 = "136bf69rwgii0wl9m1chji9778i4g0hr6073v3a2bm8n0q43x5w8";
+  };
+in
 stdenv.mkDerivation rec {
-  version = "2021-03-18";
+  version = "2021-03-30";
   name = "spicy";
   src = fetchFromGitHub {
     owner = "zeek";
     repo = "spicy";
-    rev = "9915a5f38d6dc6993984dfd93f8b441cb094b83d";
+    rev = "c5b3923ac81d8b1ef8071e0b9ba3b2cd529ca2db";
     fetchSubmodules = true;
-    sha256 = "1wnf8gh4xl1160vign87qikp0qi355lr080ip7a03cdw0r3c3v7r";
+    sha256 = "0d22880acwinrq9nyn4yhykp4fgrqckmx8rf3l3p8k2ssbllh0fh";
   };
 
 
@@ -33,6 +41,7 @@ stdenv.mkDerivation rec {
     zlib
     ninja
   ];
+
   buildInputs = [
     which
     llvmPackages.clang-unwrapped
@@ -59,6 +68,19 @@ stdenv.mkDerivation rec {
         --set CLANGPP_PATH    "${llvmPackages.clang}/bin/clang++" \
         --set LIBRARY_PATH    "${lib.makeLibraryPath [ flex bison python38 zlib glibc llvmPackages.libclang llvmPackages.libcxxabi llvmPackages.libcxx ]}"
      done
+     ${lib.optionalString SpicyPlugin
+      ''
+      cp -r ${spicy-analyzers} /build/spicy-analyzers
+      chmod 755  /build/spicy-analyzers
+      substituteInPlace /build/spicy-analyzers/CMakeLists.txt \
+      --replace "0.4" "0" \
+      --replace "00400" "0"
+      cd /build/spicy-analyzers
+      mkdir build && cd build
+      cmake -DCMAKE_INSTALL_PREFIX=$out ..
+      make -j $NIX_BUILD_CORES && make install
+      ''
+      }
   '';
 
   meta = with lib; {
